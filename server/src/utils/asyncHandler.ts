1bc45fb9195b017079afb30162f640f1183573f7
Tazim <tazim0877@gmail.com>
2025-03-30 20:50:41 +0600

    feat : create asyncHandler to handle async functions

---
+++ server/src/utils/asyncHandler.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from "express";
+
+export const asyncHandler = (requestHandler: Function) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => {
+      next(err);
+    });
+  };
+};
