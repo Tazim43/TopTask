a9e9ef0f5c6eccb2ec63fcd4025aaab0a6c04e7f
Tazim <tazim0877@gmail.com>
2025-04-05 01:17:51 +0600

    feat: create user router

---
+++ server/src/routes/User.router.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import {
+  userLogin,
+  userSignUp,
+  userLogout,
+  passwordReset,
+  refreshToken,
+} from "../controllers/User.controller";
+import { authenticate } from "src/middlewares/Auth.middleware";
+
+const router = Router();
+
+router.route("/login").post(userLogin);
+router.route("/signup").post(userSignUp);
+router.route("/logout").post(authenticate, userLogout);
+router.route("/password-reset").post(authenticate, passwordReset);
+router.route("/refresh-token").post(authenticate, refreshToken);
+
+export default router;
