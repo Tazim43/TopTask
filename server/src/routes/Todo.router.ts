18e0fa8704a602c21cfcee829c2e82f568e64e31
Tazim <tazim0877@gmail.com>
2025-04-05 14:09:25 +0600

    feat : create todo router

---
+++ server/src/routes/Todo.router.ts
@@ -0,0 +1,27 @@
+import { authenticate } from "src/middlewares/Auth.middleware";
+import {
+  createTask,
+  getTodayTasks,
+  markAsDoneById,
+  getAllDoneTasks,
+  getUpcommingTasks,
+  getOverdueTasks,
+  updateTaskById,
+  deleteTaskById,
+} from "../controllers/Todo.controller";
+
+import express from "express";
+
+const router = express.Router();
+router.use(authenticate);
+
+router.post("/", createTask);
+router.get("/done", getAllDoneTasks);
+router.get("/today", getTodayTasks);
+router.get("/upcoming", getUpcommingTasks);
+router.get("/overdue", getOverdueTasks);
+router.patch("/:id/done", markAsDoneById);
+router.put("/:id", updateTaskById);
+router.delete("/:id", deleteTaskById);
+
+export default router;
